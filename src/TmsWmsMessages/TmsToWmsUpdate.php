365d19196fc3f1b301e7f2def9a80cf87a0f1f95
Pascal Paulis <ppaulis@gmail.com>
2024-01-04 11:44:37 +0100

    refactoring
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/TmsWmsMessages/TmsToWmsUpdate.php
+++ src/TmsWmsMessages/TmsToWmsUpdate.php
@@ -7,6 +7,23 @@ class TmsToWmsUpdate
     /** @var string|null Picking Order ID */
     protected ?string $pickingOrderId = null;
 
+    public static function createFromJsonString(string $json): TmsToWmsUpdate
+    {
+        $tmsToWmsUpdate = new TmsToWmsUpdate();
+
+        foreach (json_decode($json, true) as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            $tmsToWmsUpdate->$method($value);
+        }
+
+        return $tmsToWmsUpdate;
+    }
+
+    public function serializeToJson(): string
+    {
+        return json_encode(['pickingOrderId' => $this->getPickingOrderId()]);
+    }
+
     /**
      * @return string|null
      */
