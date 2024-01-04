47baaab580480d1f245af5a4a7d73b24ccaa5923
Pascal Paulis <ppaulis@gmail.com>
2024-01-04 11:17:18 +0100

    cleanup
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/TmsWmsMessages/TmsToWmsUpdateHydrator.php
+++ src/TmsWmsMessages/TmsToWmsUpdateHydrator.php
@@ -6,7 +6,6 @@ use Laminas\Hydrator\ClassMethodsHydrator;
 
 class TmsToWmsUpdateHydrator extends ClassMethodsHydrator
 {
-
     public function __construct()
     {
         parent::__construct(false);
